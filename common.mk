fefe50bdb61f19afbf044da550ac1402221941d2
Pierre-Hugues Husson <phh@phh.me>
2019-04-19 17:29:27 +0200

    sm8250-common: Import OnePlus HAL definitions
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -442,8 +442,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    device/oneplus/common
+    $(LOCAL_PATH)
 
 # Surface Flinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
