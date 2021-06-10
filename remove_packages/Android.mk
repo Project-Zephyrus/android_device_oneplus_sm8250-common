8b31674938f6f187b652eddc1153e3e40e062f88
chandu078 <chandudyavanapelli03@gmail.com>
2021-06-10 20:13:47 +0530

    sm8250-common: Add OpDolby * Thanks to Omkar Chandorkar for sepolicy.
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Camera2 GalleryGoPrebuilt Gallery2 NfcNci RecorderPrebuilt SafetyHubPrebuilt Snap
+LOCAL_OVERRIDES_PACKAGES := Camera2 GalleryGoPrebuilt Gallery2 NfcNci RecorderPrebuilt SafetyHubPrebuilt Snap MusicFX
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
