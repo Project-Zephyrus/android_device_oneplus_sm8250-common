4d26d18dcbfd091481098bfbca688bc58bea6de4
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-09-18 04:39:47 +0000

    Init Dot

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CLANG_VERSION := proton
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
