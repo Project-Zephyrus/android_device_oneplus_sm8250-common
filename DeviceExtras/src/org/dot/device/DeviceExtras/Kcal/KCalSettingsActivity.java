kunalshah912/device_oneplus_sm8250-common

sm8250-common: Introduce DeviceExtras!
- Awesome alert slider configs
- Ambient display gestures (doze merged into DeviceExtras) + QS tile
- DC-Dimming + QS tile
- FPS Overlay + QS tile
- HBM & Auto HBM + QS tile
- Kcal display calibration & presets + QS tile
- Force GameMode (240hz touch sampling) toggle + QS
- Force USB 2.0 FastCharge toggle + QS
- Vibration strength preference

- Screenshots:
https://imgur.com/gallery/nA83aR2

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
Co-authored-by: Adrian DC <radian.dc@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

DeviceExtras/src/org/evolution/device/DeviceExtras/Kcal/KCalSettingsActivity.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2018 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.evolution.device.DeviceExtras.kcal;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import org.evolution.device.DeviceExtras.R;
+
+public class KCalSettingsActivity extends Activity implements KCalUtils {
+
+    private KCalSettings mKCalSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kcal);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_kcal);
+        if (fragment == null) {
+            mKCalSettingsFragment = new KCalSettings();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_kcal, mKCalSettingsFragment)
+                    .commit();
+        } else {
+            mKCalSettingsFragment = (KCalSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+
+            case R.id.action_reset:
+                mKCalSettingsFragment.applyValues(RED_DEFAULT + " " +
+                        GREEN_DEFAULT + " " +
+                        BLUE_DEFAULT + " " +
+                        SATURATION_DEFAULT + " " +
+                        VALUE_DEFAULT + " " +
+                        CONTRAST_DEFAULT + " " +
+                        HUE_DEFAULT);
+                return true;
+
+            case R.id.action_preset:
+                new PresetDialog().show(getFragmentManager(),
+                        KCalSettingsActivity.class.getName(), mKCalSettingsFragment);
+                return true;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_reset, menu);
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Kcal/KCalUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2018 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.evolution.device.DeviceExtras.kcal;
+
+public interface KCalUtils {
+    String PREF_ENABLED = "kcal_enabled";
+    String PREF_RED = "color_red";
+    String PREF_GREEN = "color_green";
+    String PREF_BLUE = "color_blue";
+    String PREF_SATURATION = "saturation";
+    String PREF_VALUE = "value";
+    String PREF_CONTRAST = "contrast";
+    String PREF_HUE = "hue";
+
+    int RED_DEFAULT = 256;
+    int GREEN_DEFAULT = 256;
+    int BLUE_DEFAULT = 256;
+    int SATURATION_DEFAULT = 127;
+    int SATURATION_OFFSET = 128;
+    int VALUE_DEFAULT = 127;
+    int VALUE_OFFSET = 128;
+    int CONTRAST_DEFAULT = 127;
+    int CONTRAST_OFFSET = 128;
+    int HUE_DEFAULT = 0;
+
+    String COLOR_FILE = "/sys/module/msm_drm/parameters";
+    String COLOR_FILE_RED = COLOR_FILE + "/kcal_red";
+    String COLOR_FILE_GREEN = COLOR_FILE + "/kcal_green";
+    String COLOR_FILE_BLUE = COLOR_FILE + "/kcal_blue";
+    String COLOR_FILE_SAT = COLOR_FILE + "/kcal_sat";
+    String COLOR_FILE_HUE = COLOR_FILE + "/kcal_hue";
+    String COLOR_FILE_VAL = COLOR_FILE + "/kcal_val";
+    String COLOR_FILE_CONT = COLOR_FILE + "/kcal_cont";
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Services/GameModeTileService.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+* Copyright (C) 2021 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.device.DeviceExtras.DeviceExtras;
+
+@TargetApi(24)
+public class GameModeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled =GameModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = GameModeSwitch.isCurrentlyEnabled(this);
+        FileUtils.writeValue(GameModeSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceExtras.KEY_GAME_SWITCH, enabled ? false : true).commit();
+        //getQsTile().setLabel(enabled ? "Game Mode off" : "Game Mode On");
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Services/HBMModeTileService.java
@@ -1,0 +1,71 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+@TargetApi(24)
+public class HBMModeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        FileUtils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "5");
+        sharedPrefs.edit().putBoolean(DeviceExtras.KEY_HBM_SWITCH, enabled ? false : true).commit();
+        //getQsTile().setLabel(enabled ? "HBM off" : "HBM On");
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Startup.java
@@ -1,0 +1,77 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2021 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.text.TextUtils;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_DC_SWITCH, false);
+        if (enabled) {
+        restore(DCModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+        restore(GameModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_USB2_SWITCH, false);
+        if (enabled) {
+        restore(USB2FastChargeModeSwitch.getFile(), enabled);
+       }
+        DeviceExtras.restoreSliderStates(context);
+        org.evolution.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
+        org.evolution.device.DeviceExtras.kcal.KCalSettings.restore(context);
+        VibratorStrengthPreference.restore(context);
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}

Project-Zephyrus/android_device_oneplus_sm8250-common

DeviceExtras/src/org/dot/device/DeviceExtras/Kcal/KCalSettingsActivity.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2018 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.dot.device.DeviceExtras.kcal;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import org.dot.device.DeviceExtras.R;
+
+public class KCalSettingsActivity extends Activity implements KCalUtils {
+
+    private KCalSettings mKCalSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kcal);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_kcal);
+        if (fragment == null) {
+            mKCalSettingsFragment = new KCalSettings();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_kcal, mKCalSettingsFragment)
+                    .commit();
+        } else {
+            mKCalSettingsFragment = (KCalSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+
+            case R.id.action_reset:
+                mKCalSettingsFragment.applyValues(RED_DEFAULT + " " +
+                        GREEN_DEFAULT + " " +
+                        BLUE_DEFAULT + " " +
+                        SATURATION_DEFAULT + " " +
+                        VALUE_DEFAULT + " " +
+                        CONTRAST_DEFAULT + " " +
+                        HUE_DEFAULT);
+                return true;
+
+            case R.id.action_preset:
+                new PresetDialog().show(getFragmentManager(),
+                        KCalSettingsActivity.class.getName(), mKCalSettingsFragment);
+                return true;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_reset, menu);
+        return true;
+    }
+}

DeviceExtras/src/org/dot/device/DeviceExtras/Kcal/KCalUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2018 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.dot.device.DeviceExtras.kcal;
+
+public interface KCalUtils {
+    String PREF_ENABLED = "kcal_enabled";
+    String PREF_RED = "color_red";
+    String PREF_GREEN = "color_green";
+    String PREF_BLUE = "color_blue";
+    String PREF_SATURATION = "saturation";
+    String PREF_VALUE = "value";
+    String PREF_CONTRAST = "contrast";
+    String PREF_HUE = "hue";
+
+    int RED_DEFAULT = 256;
+    int GREEN_DEFAULT = 256;
+    int BLUE_DEFAULT = 256;
+    int SATURATION_DEFAULT = 127;
+    int SATURATION_OFFSET = 128;
+    int VALUE_DEFAULT = 127;
+    int VALUE_OFFSET = 128;
+    int CONTRAST_DEFAULT = 127;
+    int CONTRAST_OFFSET = 128;
+    int HUE_DEFAULT = 0;
+
+    String COLOR_FILE = "/sys/module/msm_drm/parameters";
+    String COLOR_FILE_RED = COLOR_FILE + "/kcal_red";
+    String COLOR_FILE_GREEN = COLOR_FILE + "/kcal_green";
+    String COLOR_FILE_BLUE = COLOR_FILE + "/kcal_blue";
+    String COLOR_FILE_SAT = COLOR_FILE + "/kcal_sat";
+    String COLOR_FILE_HUE = COLOR_FILE + "/kcal_hue";
+    String COLOR_FILE_VAL = COLOR_FILE + "/kcal_val";
+    String COLOR_FILE_CONT = COLOR_FILE + "/kcal_cont";
+}

DeviceExtras/src/org/dot/device/DeviceExtras/Services/GameModeTileService.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+* Copyright (C) 2021 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.dot.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.dot.device.DeviceExtras.DeviceExtras;
+
+@TargetApi(24)
+public class GameModeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled =GameModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = GameModeSwitch.isCurrentlyEnabled(this);
+        FileUtils.writeValue(GameModeSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceExtras.KEY_GAME_SWITCH, enabled ? false : true).commit();
+        //getQsTile().setLabel(enabled ? "Game Mode off" : "Game Mode On");
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceExtras/src/org/dot/device/DeviceExtras/Services/HBMModeTileService.java
@@ -1,0 +1,71 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.dot.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+@TargetApi(24)
+public class HBMModeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        FileUtils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "5");
+        sharedPrefs.edit().putBoolean(DeviceExtras.KEY_HBM_SWITCH, enabled ? false : true).commit();
+        //getQsTile().setLabel(enabled ? "HBM off" : "HBM On");
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceExtras/src/org/dot/device/DeviceExtras/Startup.java
@@ -1,0 +1,77 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2021 The dot OS Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.dot.device.DeviceExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.text.TextUtils;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_DC_SWITCH, false);
+        if (enabled) {
+        restore(DCModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+        restore(GameModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_USB2_SWITCH, false);
+        if (enabled) {
+        restore(USB2FastChargeModeSwitch.getFile(), enabled);
+       }
+        DeviceExtras.restoreSliderStates(context);
+        org.dot.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
+        org.dot.device.DeviceExtras.kcal.KCalSettings.restore(context);
+        VibratorStrengthPreference.restore(context);
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}
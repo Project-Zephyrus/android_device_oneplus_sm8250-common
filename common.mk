71f16ffd1d63b223b71bd1fb0a19f774d39c9564
AnierinB <anierin@evolution-x.org>
2019-05-09 00:44:57 +0530

    sm8250-common: Introduce DeviceExtras!
    - Awesome alert slider configs
    - Ambient display gestures (doze merged into DeviceExtras) + QS tile
    - DC-Dimming + QS tile
    - FPS Overlay + QS tile
    - HBM & Auto HBM + QS tile
    - Kcal display calibration & presets + QS tile
    - Force GameMode (240hz touch sampling) toggle + QS
    - Force USB 2.0 FastCharge toggle + QS
    - Vibration strength preference
    
    - Screenshots:
    https://imgur.com/gallery/nA83aR2
    
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: Adrian DC <radian.dc@gmail.com>
    Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
    Co-authored-by: Michele Bono <bono.michele94@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

--- common.mk
+++ common.mk
@@ -242,6 +242,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/misc/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/misc/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
+# DeviceExtras Package
+PRODUCT_PACKAGES += \
+    DeviceExtras
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
@@ -278,10 +282,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
-
-# Doze
-PRODUCT_PACKAGES += \
-    OnePlusDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -471,7 +471,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Update engine
