1f2361c4f7b56b1733c8bd47231dbe842dcab163
Omar Hamad <etahamad@icloud.com>
2021-10-08 01:42:56 +0200

    sm8250-common: Build libjson.so from source
    Track https://github.com/LineageOS/android_external_json-c

--- common.mk
+++ common.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#libjson
+$(call inherit-product, external/json-c/Android.configure.mk)
 
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
