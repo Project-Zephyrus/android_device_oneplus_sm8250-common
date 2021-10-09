d01572cd7dd176e937c39a0209fa44bc49ab2d95
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-10-09 18:21:43 +0000

    sm8250-common: Set VNDK to 30 and also add bunch of prebuilts
    since we are still on R vendor

--- common.mk
+++ common.mk
@@ -47,6 +47,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # VNDK
 PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -499,6 +501,56 @@ PRODUCT_PACKAGES += \
 # Vendor libstdc++
 PRODUCT_PACKAGES += \
     libstdc++.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.gnss@2.1 \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss.measurement_corrections@1.1 \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0 \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gatekeeper@1.0 \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.1 \
+    android.hardware.keymaster@4.1.vendor \
+    android.system.net.netd@1.1.vendor \
+    android.hardware.neuralnetworks@1.2 \
+    android.hardware.neuralnetworks@1.2.vendor \
+    android.hardware.secure_element@1.2 \
+    android.hardware.secure_element@1.2.vendor\
+    android.hardware.radio@1.5 \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.nfc@1.0 \
+    android.hardware.nfc@1.0.vendor \
+    android.hardware.authsecret@1.0 \
+    android.hardware.authsecret@1.0.vendor \
+    android.hardware.drm@1.0 \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1 \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2 \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3 \
+    android.hardware.drm@1.3.vendor \
+    android.hardware.nfc@1.1 \
+    android.hardware.nfc@1.1.vendor \
+    android.hardware.nfc@1.2 \
+    android.hardware.nfc@1.2.vendor \
+    android.hardware.nfc@1.3 \
+    android.hardware.nfc@1.3.vendor \
+    android.hardware.neuralnetworks@1.0 \
+    android.hardware.neuralnetworks@1.0.vendor \
+    android.hardware.neuralnetworks@1.1 \
+    android.hardware.neuralnetworks@1.1.vendor \
+    android.hardware.neuralnetworks@1.3 \
+    android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor
 
 # Vibrator
 PRODUCT_PACKAGES += \
