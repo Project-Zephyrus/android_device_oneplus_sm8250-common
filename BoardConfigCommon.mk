a9c1ffbd78a2b168896fa2d52474278eb7fa84b0
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-12-31 10:35:12 +0000

    sm8250-common: sepolicy: Address More denials

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,6 +191,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
