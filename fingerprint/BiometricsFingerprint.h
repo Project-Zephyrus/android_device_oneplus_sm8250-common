e1873d95a28754968e89aabd3918790f8c36419d
alk3pInjection <webmaster@raspii.tech>
2022-01-22 12:34:45 +0800

    sm8250-commmon: Udfps: General improvements
    * Fixes the following scenarios:
    
    - The flicker upon turning off the panel is gone.
    
    - Set "lock after screen timeout" to a higher value,
      then let the screen turn off by timeout and wake it up.
      The dimlayer will not appear.
    
    ziasam: Also add sepolicy rule
    
    Reported-by: Pranav <npv12@iitbbs.ac.in>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Co-authored-by: ziasam <ziauddinsameer@gmail.com>
    Change-Id: Ia7bba1d2e58bf46d66b3a424a1fc233299acc98b

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -82,6 +82,7 @@ private:
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
     static FingerprintAcquiredInfo VendorAcquiredFilter(int32_t error, int32_t* vendorCode);
     static BiometricsFingerprint* sInstance;
+    int isCancelled = 0;
 
     std::mutex mClientCallbackMutex;
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
