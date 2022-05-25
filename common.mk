pe-libxzr/device_oneplus_sm8250-common

sm8250-common: Introduce charge control HAL
Signed-off-by: LibXZR <i@xzr.moe>

common.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.libxzr.chgctrl@1.0-service.oneplus_kona
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     ftm_power_config.sh \

Project-Zephyrus/android_device_oneplus_sm8250-common

Change-Id: I7441eebc675a957a10138d5c448fc45c872883d0
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
Signed-off-by: Manikantaraavi <raavimanikanta6595@gmail.com>

common.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.kscope.chgctrl@1.0-service.oneplus_kona
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \