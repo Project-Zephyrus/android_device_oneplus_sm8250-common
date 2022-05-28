1d3f36c13d3a015563313da3baa0ad5a79ebdd9d
Ziauddin Sameer <ziauddinsameer@gmail.com>
2022-05-28 00:19:46 +0000

    sm8250-common: Init LOS

--- common.mk
+++ common.mk
@@ -42,7 +42,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-dot
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     SettingsOverlayRefreshRate
